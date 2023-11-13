9a989d49f57db1a1708e71d294695ed5d427fb26
Risanda601 <146081131+Risanda601@users.noreply.github.com>
2023-11-13 14:56:02 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,3 @@
-let timer = 0
-let lemon_on_spoon = false
-let lemon: game.LedSprite = null
-input.onButtonPressed(Button.A, function () {
-    basic.showString("3 2 1  go!")
-    timer = 0
-    lemon_on_spoon = true
-    lemon = game.createSprite(2, 2)
-    while (lemon_on_spoon) {
-        let sprite = 0
-        timer += 1
-        if (input.acceleration(Dimension.X) > 200) {
-        	
-        }
-        if (sprite) {
-        	
-        } else {
-        	
-        }
-    }
-})
 basic.forever(function () {
 	
 })
